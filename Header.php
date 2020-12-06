73fcee71130a1b0dc3006c2711067e7c689791b4
Huy Thang Tran Van <huythang@web.de>
2020-12-06 17:13:16 +0100

    Projekte

---
+++ Header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Meine Startseite</title>
+    <link href="https://unpkg.com/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          rel="stylesheet" />
+
+</head>
+<body>
+
+        <h1>Aufgabenplaner :</h1>
+
+</body>
+
+</html>
---
+++ Sidebar.php
@@ -0,0 +1,32 @@
+<!Doctype html>
+<html lang ="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Log in</title>
+    <link href="https://unpkg.com/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          rel="stylesheet" />
+
+</head>
+<body>
+                <ul class="list-group">
+                <li class="list-group-item">
+                    <a class="nav-link Log in" href="Übung2.php"> Login</a>
+                </li>
+                <li class="list-group-item">
+                    <a class="nav-link Log in" href="Projekte.php">
+                        Projekte </a>
+                </li>
+                </li>
+                <li class="list-group-item">   <a class="nav-link Log in" href="Aufgabe_1.php"> Aktuelles Projekt </a>
+
+                </li>
+                <li class="list-group-item ml-5">   <a class="nav-link Log in" href="Reiter.php"> Reiter </a>
+
+                </li>
+                <li class="list-group-item ml-5">   <a class="nav-link Log in" href="Aufgaben.php"> Aufgaben </a>
+                </li>
+                <li class="list-group-item ml-5">   <a class="nav-link Log in" href="Mitglieder.php"> Mitglieder </a>
+
+                </li>
+</body>
+</html>
